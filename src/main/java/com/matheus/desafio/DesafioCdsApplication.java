ac1e13efe5aaf07832cec69b6799c9f7edb46d4d
matheusaguiar <matheusvarelo@hotmail.com>
2018-03-02 15:17:08 -0300

    Projeto criado

---
+++ src/main/java/com/matheus/desafio/DesafioCdsApplication.java
@@ -0,0 +1,12 @@
+package com.matheus.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCdsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/matheus/desafio/DesafioCdsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.matheus.desafio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesafioCdsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
