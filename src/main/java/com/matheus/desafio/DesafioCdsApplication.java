c14ff010702cb2cf09880a873ea3597d2ec3ade3
matheusaguiar <matheusvarelo@hotmail.com>
2018-03-03 15:02:17 -0300

    Criando operaçao de instanciacao

--- src/main/java/com/matheus/desafio/DesafioCdsApplication.java
+++ src/main/java/com/matheus/desafio/DesafioCdsApplication.java
@@ -1,12 +1,34 @@
 package com.matheus.desafio;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.matheus.desafio.domain.Categoria;
+import com.matheus.desafio.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class DesafioCdsApplication {
+public class DesafioCdsApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioCdsApplication.class, args);
 	}
+ 
+	@Override
+	public void run(String... arg0) throws Exception {
+		
+		Categoria cat1 =new Categoria(null,"Carlos","125152185");
+		
+		categoriaRepository.save(Arrays.asList(cat1));
+		
+		
+	}
+	
+	
 }
